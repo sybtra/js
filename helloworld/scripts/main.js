5a1aecd2375fefc3a7f5ad1dd102ba104fd10430
sybtra <tbandiougou3@gmail.com>
2022-08-02 16:33:32 +0400

    continuing helloworld

--- helloworld/scripts/main.js
+++ helloworld/scripts/main.js
@@ -1,2 +1,15 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello World!';
+let num1 = 5;
+let num2 = 3;
+const myp = document.querySelector('p');
+myp.textContent = num1 + ' multiplies to ' + num2 + ' equals ' + multiply(num1,num2) + '.';
+
+document.querySelector('h1').addEventListener('cliak', function() {
+    alert('Ouch! Stop poking me!');
+});
+
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
+}
