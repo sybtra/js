169b83b1398109eb81ef733972d16cb990b7df6f
sybtra <tbandiougou3@gmail.com>
2022-08-03 13:03:37 +0400

    alternating image display in dem

--- helloworld/scripts/main.js
+++ helloworld/scripts/main.js
@@ -1,15 +1,12 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World!';
-let num1 = 5;
-let num2 = 3;
-const myp = document.querySelector('p');
-myp.textContent = num1 + ' multiplies to ' + num2 + ' equals ' + multiply(num1,num2) + '.';
-
-document.querySelector('h1').addEventListener('cliak', function() {
-    alert('Ouch! Stop poking me!');
-});
-
-function multiply(num1, num2) {
-    let result = num1 * num2;
-    return result;
+let myImage = document.querySelector('img');
+myImage.setAttribute('alt','initial image');
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src','images/firefox2.png');
+        myImage.setAttribute('alt', 'my image is firefox2.png');
+    } else {
+        myImage.setAttribute('src','images/firefox-icon.png');
+        myImage.setAttribute('alt', 'my image is firefox-icon.png');
+    }
 }
