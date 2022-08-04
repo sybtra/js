d153628e2ebc4d2a73310af0d54745e364f500f6
sybtra <tbandiougou3@gmail.com>
2022-08-04 13:35:00 +0400

    guess number game improvement

--- guess-number-game/scripts/main.js
+++ guess-number-game/scripts/main.js
@@ -7,7 +7,7 @@ const guessSubmit = document.querySelector('.guessSubmit');
 const guessField = document.querySelector('.guessField');
 let guessCount = 1;
 let resetButton;
-
+guessField.focus();
 function checkGuess() {
     const userGuess = Number(guessField.value);
     if (guessCount === 1) {
